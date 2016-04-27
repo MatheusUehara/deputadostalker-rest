2a62d17a841a9d83eb2eca1d3476eaea38ac57ce
Matheus Uehara <matheus.pitanco.nti@ufpe.br>
2016-04-27 19:00:10 -0300

    Commit com inserção de deputados

--- deputado/index.php
+++ deputado/index.php
@@ -1,36 +1,107 @@
 <?php
-require_once '../libs/vendor/autoload.php';
- 
-$app = new \Slim\Slim();
-$app->get('/:name', function ($name) {
-    echo "Hello, $name"."<br>";
-});
-$app->run();
+    require_once '../libs/vendor/autoload.php';
+    require_once '../include/DbConnect.php';
 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => 'http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDeputados',
-    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
-));
-$resp = curl_exec($curl);
-curl_close($curl);
+     
+    $app = new \Slim\Slim();
+    $app->get('/:name', function ($name) {
+        echo "Hello, $name"."<br>";
+    });
+    $app->run();  
 
-$xml_object = simplexml_load_string($resp);
+    function obterDetalhesDeputado($ideCadastro,$numLegislatura){
+        $curl = curl_init();        
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDetalhesDeputado?ideCadastro='.$ideCadastro.'&numLegislatura='.$numLegislatura,
+            CURLOPT_USERAGENT => 'Deputado Stalker postRequest',
+        ));
 
-$json = json_encode(simplexml_load_string($resp));
+        $resp = curl_exec($curl);
+        curl_close($curl);
 
-$obj = json_decode($json);
+        $xml_object = simplexml_load_string($resp);
 
-foreach($obj -> deputado as $item){
-    echo "<img src= '" . $item -> urlFoto ."'/>"."<br />";
-    echo "<strong>TÌtulo:</strong> ".utf8_decode($item -> ideCadastro)."<br />";
-    echo "<strong>Link:</strong> ".utf8_decode($item -> nome)."<br />";
-    echo "<strong>DescriÁ„o:</strong> ".utf8_decode($item -> nomeParlamentar)."<br />";
-    echo "<strong>Autor:</strong> ".utf8_decode($item -> sexo)."<br />";
-    echo "<strong>Data:</strong> ".utf8_decode($item -> partido)."<br />";
-    echo "<br />";
-}
+        $json = json_encode(simplexml_load_string($resp));
 
-?>
+        $obj = json_decode($json);
 
+        print_r($obj);
+
+
+
+    }
+
+    function obterDeputados(){
+
+        $db = new DbConnect();
+        $conn = $db->connect();
+
+
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDeputados',
+            CURLOPT_USERAGENT => 'Deputado Stalker getRequest'
+        ));
+        $resp = curl_exec($curl);
+        curl_close($curl);
+
+        $xml_object = simplexml_load_string($resp);
+
+        $json = json_encode(simplexml_load_string($resp));
+
+        $obj = json_decode($json);
+
+
+        foreach($obj -> deputado as $item){
+
+            $ideCadastro = $item-> ideCadastro;
+            $matricula = $item-> matricula;
+            $idParlamentar = $item-> idParlamentar;
+            $nomeCivil = utf8_encode($item-> nome);
+            $nomeParlamentar = utf8_encode($item-> nomeParlamentar);
+            $urlFoto = utf8_encode($item-> urlFoto);
+            $sexo = utf8_encode($item-> sexo);
+            $ufRepresentacaoAtual = utf8_encode($item-> uf);
+            $email = utf8_encode($item-> email);
+
+            $stmt = $conn->prepare("INSERT INTO deputado(ideCadastro, matricula, idParlamentar, nomeCivil, nomeParlamentar, urlFoto, sexo, ufRepresentacaoAtual, email) 
+                values (? , ? , ? , ? , ? , ? , ? , ? , ? )");
+
+            $stmt->bind_param( "iiissssss", $ideCadastro,$matricula,$idParlamentar,$nomeCivil,$nomeParlamentar,$urlFoto,$sexo,$ufRepresentacaoAtual,$email);
+
+            $result = $stmt->execute();
+
+            $stmt->close();
+
+            // Check for successful insertion
+            if ($result) {
+                // User successfully inserted
+                echo DEPUTADO_INSERIDO_COM_SUCESSO;
+            } else {
+                // Failed to create user
+                echo DEPUTADO_FALHA_INSERIR;
+            }
+
+
+            /* ----------------- Aqui √© um debug caso seja necess√°rio visualizar o que est√° sendo retornado ----------------------- */
+            /*
+            echo "<img src= '" . $item -> urlFoto ."'/>"."<br />";
+            echo "<strong>Ide Cadastro:</strong> ".$item -> ideCadastro."<br />";
+            echo "<strong>Nome:</strong> ".$item -> nome."<br />";
+            echo "<strong>Nome Parlamentar:</strong> ".$item -> nomeParlamentar."<br />";
+            echo "<strong>Sexo:</strong> ".$item -> sexo."<br />";
+            echo "<strong>Partido:</strong> ".$item -> partido."<br />";
+            echo "<br />";
+            */
+        }
+        
+    }
+
+    obterDeputados();
+    //obterDetalhesDeputado(160976,55);
+    
+
+?> 
--- include/Config.php
+++ include/Config.php
@@ -5,8 +5,11 @@
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'task_manager');
+define('DB_NAME', 'deputadostalker');
 
+define('DEPUTADO_INSERIDO_COM_SUCESSO',0);
+define('DEPUTADO_ATUALIZADO_COM_SUCESSO',0);
+define('DEPUTADO_FALHA_INSERIR',1);
 define('USER_CREATED_SUCCESSFULLY', 0);
 define('USER_CREATE_FAILED', 1);
 define('USER_ALREADY_EXISTED', 2);

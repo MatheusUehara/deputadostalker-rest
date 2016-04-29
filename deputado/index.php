40f007e2dd8f1cc7a0e09bc1fcdaff2ba985a69e
Matheus Uehara <matheus.pitanco.nti@ufpe.br>
2016-04-29 18:48:57 -0300

    Commit com adição do header

--- deputado/index.php
+++ deputado/index.php
@@ -1,20 +1,28 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
+<!DOCTYPE HTML>
+<html lang='pt-BR'>
+    <head>
+        <title>Deputado Route</title>
+    </head>
+    <body>
+        <?php
+            header('Content-Type: text/html; charset=utf-8');
+            require_once '../libs/vendor/autoload.php';
+            $app = new \Slim\Slim();
 
-require_once '../libs/vendor/autoload.php';
+            $app->get('/:name', function ($name) {
+                echo "Hello , $name"."<br>";
+            });
 
-    $app = new \Slim\Slim();
-    $app->get('/:name', function ($name) {
-        echo "Hello , $name"."<br>";
-    });
+            $app->get('/atualizarBaseDeputados/', function () {
+                require_once 'Deputado.php';
+                $obterdeputado = new Deputado();
+                // execução de obtençao de detalhes de um deputado especifico
+                //$obterdeputado->inserirDetalhesDeputado(160617,55);
+                $obterdeputado->obterDeputados();
+                echo "Base preenchida com sucesso!";
+            });
 
-    $app->get('/atualizarBaseDeputados/', function () {
-        require_once 'Deputado.php';
-        $obterdeputado = new Deputado();
-        // execução de obtençao de detalhes de um deputado especifico
-        //$obterdeputado->inserirDetalhesDeputado(160617,55);
-        $obterdeputado->obterDeputados();
-        echo "Base preenchida com sucesso!";
-    });
-    $app->run();
-?>
+            $app->run();
+        ?>
+    </body>
+</html>

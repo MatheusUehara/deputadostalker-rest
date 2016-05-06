0a1e818df43f6bc04e41b1a2f9bf8334771bb520
MatheusUehara <matheuspitanco@gmail.com>
2016-05-05 21:40:29 -0300

    opdkasodpk

--- deputado/index.php
+++ deputado/index.php
@@ -1,82 +1,68 @@
-<!DOCTYPE HTML>
-<html lang='pt-BR'>
-    <head>
-        <title>Deputado Route</title>
-    </head>
-    <body>
-        <?php
-            require_once 'Deputado.php';
+<?php
+    require_once 'Deputado.php';
+    require_once '../libs/vendor/autoload.php';
+    $app = new \Slim\Slim();
 
-            header('Content-Type: text/html; charset=utf-8');
-            require_once '../libs/vendor/autoload.php';
-            $app = new \Slim\Slim();
-
-
-            /*
-            * Primeira rota criada afim de testar os serviços
-            */
-            $app->get('/:name', function ($name) {
-                echo "Hello , $name"."<br>";
-            });
+    /*
+    * Primeira rota criada afim de testar os serviços
+    */
+    $app->get('/:name', function ($name) {
+        echo "Hello , $name"."<br>";
+    });
 
 
-            /*
-            * Preenche/Atualiza a base de dados com os deputados
-            */
-            $app->get('/atualizarBaseDeputados/', function () {
-                $deputado = new Deputado();
-                $deputado->obterDeputadosCamara();
-                echo "Base preenchida com sucesso!";
-            });
+    /*
+    * Preenche/Atualiza a base de dados com os deputados
+    */
+    $app->get('/atualizarBaseDeputados/', function () {
+        $deputado = new Deputado();
+        $deputado->obterDeputadosCamara();
+        echo "Base preenchida com sucesso!";
+    });
 
-            /*
-            * Retorna todos os deputados presentes na base de dados
-            */
-            $app->get('/getDeputados/', function() {
-                $deputado = new Deputado();
-                $response = array();
-                $result = $deputado->getDeputados();
+    /*
+    * Retorna todos os deputados presentes na base de dados
+    */
+    $app->get('/getDeputados/', function() {
+        $deputado = new Deputado();
+        $response = array();
+        $result = $deputado->getDeputados();
+        $response = array();
+        while ($deputados = $result->fetch_assoc()) {
+            $tmp = array();
+            $tmp["ideCadastro"] = $deputados["ideCadastro"];
+            $tmp["matricula"] = $deputados["matricula"];
+            $tmp["idParlamentar"] = $deputados["idParlamentar"];
+            $tmp["nomeCivil"] = $deputados["nomeCivil"];
+            $tmp["nomeParlamentar"] = $deputados["nomeParlamentar"];
+            $tmp["urlFoto"] = $deputados["urlFoto"];
+            $tmp["sexo"] = $deputados["sexo"];
+            $tmp["ufRepresentacaoAtual"] = $deputados["ufRepresentacaoAtual"];
+            $tmp["email"] = $deputados["email"];
+            $tmp["dataNascimento"] = $deputados["dataNascimento"];
+            $tmp["nomeProfissao"] = $deputados["nomeProfissao"];
+            $tmp["situacaoLegislaturaAtual"] = $deputados["situacaoLegislaturaAtual"];
+            $tmp["gabinete_idGabinete"] = $deputados["gabinete_idGabinete"];
+            $tmp["partido_idPartido"] = $deputados["partido_idPartido"];
+            array_push($response, $tmp);
+        }
+        echoRespnse(200, $response);
+    });
 
-                //$response["error"] = false;
-                $response = array();
+    /**
+     * Echoing json response to client
+     * @param String $status_code Http response code
+     * @param Int $response Json response
+     */
+    function echoRespnse($status_code, $response) {
+        $app = \Slim\Slim::getInstance();
+        // Http response code
+        $app->status($status_code);
 
-                while ($deputados = $result->fetch_assoc()) {
-                    $tmp = array();
-                    $tmp["ideCadastro"] = $deputados["ideCadastro"];
-                    $tmp["matricula"] = $deputados["matricula"];
-                    $tmp["idParlamentar"] = $deputados["idParlamentar"];
-                    $tmp["nomeCivil"] = $deputados["nomeCivil"];
-                    $tmp["nomeParlamentar"] = $deputados["nomeParlamentar"];
-                    $tmp["urlFoto"] = $deputados["urlFoto"];
-                    $tmp["sexo"] = $deputados["sexo"];
-                    $tmp["ufRepresentacaoAtual"] = $deputados["ufRepresentacaoAtual"];
-                    $tmp["email"] = $deputados["email"];
-                    $tmp["dataNascimento"] = $deputados["dataNascimento"];
-                    $tmp["nomeProfissao"] = $deputados["nomeProfissao"];
-                    $tmp["situacaoLegislaturaAtual"] = $deputados["situacaoLegislaturaAtual"];
-                    $tmp["gabinete_idGabinete"] = $deputados["gabinete_idGabinete"];
-                    $tmp["partido_idPartido"] = $deputados["partido_idPartido"];
-                    array_push($response, $tmp);
-                }
-                echoRespnse(200, $response);
-            });
+        // setting response content type to json
+        $app->contentType('json');
 
-            /**
-             * Echoing json response to client
-             * @param String $status_code Http response code
-             * @param Int $response Json response
-             */
-            function echoRespnse($status_code, $response) {
-                $app = \Slim\Slim::getInstance();
-                // Http response code
-                $app->status($status_code);
-
-                // setting response content type to json
-                $app->contentType('json');
-
-                echo json_encode($response);
-            }
-            $app->run();
-        ?>
-    </body>
-</html>
+        echo json_encode($response);
+    }
+    $app->run();
+?>
--- partido/index.php
+++ partido/index.php
@@ -1,52 +1,43 @@
-<!DOCTYPE HTML>
-<html lang='pt-BR'>
-    <head>
-        <title>Partido Route</title>
-    </head>
-    <body>
-        <?php
-            require_once 'Partido.php';
-            header('Content-Type: text/html; charset=utf-8');
-            require_once '../libs/vendor/autoload.php';
-            $app = new \Slim\Slim();
+<?php
+    require_once 'Partido.php';
+    require_once '../libs/vendor/autoload.php';
+    $app = new \Slim\Slim();
 
-            $app->get('/getPartidos/', function() {
-                $partido = new Partido();
-                $response = array();
-                $result = $partido->getPartidos();
+    $app->get('/getPartidos/', function() {
+        $partido = new Partido();
+        $response = array();
+        $result = $partido->getPartidos();
 
-                
-                $response = array();
-
-                // looping through result and preparing tasks array
-                while ($partidos = $result->fetch_assoc()) {
-                    $tmp = array();
-                    $tmp["idPartido"] = $partidos["idPartido"];
-                    $tmp["nome"] = $partidos["nome"];
-                    array_push($response, $tmp);
-                }
-
-                echoRespnse(200, $response);
-            });
         
+        $response = array();
+
+        // looping through result and preparing tasks array
+        while ($partidos = $result->fetch_assoc()) {
+            $tmp = array();
+            $tmp["idPartido"] = $partidos["idPartido"];
+            $tmp["nome"] = $partidos["nome"];
+            array_push($response, $tmp);
+        }
+
+        echoRespnse(200, $response);
+    });
 
 
-            /**
-             * Echoing json response to client
-             * @param String $status_code Http response code
-             * @param Int $response Json response
-             */
-            function echoRespnse($status_code, $response) {
-                $app = \Slim\Slim::getInstance();
-                // Http response code
-                $app->status($status_code);
-    
-                // setting response content type to json
-                $app->contentType('json');
-    
-                echo json_encode($response);
-            }
-            $app->run();
-        ?>
-    </body>
-</html>
+
+    /**
+     * Echoing json response to client
+     * @param String $status_code Http response code
+     * @param Int $response Json response
+     */
+    function echoRespnse($status_code, $response) {
+        $app = \Slim\Slim::getInstance();
+        // Http response code
+        $app->status($status_code);
+
+        // setting response content type to json
+        $app->contentType('json');
+
+        echo json_encode($response);
+    }
+    $app->run();
+?>
